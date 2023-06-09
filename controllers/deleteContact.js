9d48dbbe008d27044cd05589dd7df7d388073bf3
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-06-09 23:16:55 +0300

    fix: структура файлів

---
+++ controllers/deleteContact.js
@@ -0,0 +1,26 @@
+const Contact = require("../models/contacts");
+
+const { HttpError } = require('../helpers');
+
+const {ctrlWrapper} = require("../decorators")
+const  isValidId   = require("../middelwares/isValidId");
+
+const deleteContact = async (req, res) => {
+  
+  const { contactId } = req.params;
+  isValidId(contactId);
+    const result = await Contact.findByIdAndDelete (contactId);
+    if (!result) {
+      throw HttpError(404, `Contacts with ${contactId} not found`);
+    }
+      res.json({
+      message:"Delete success"
+    })
+}
+
+     
+module.exports = {
+ 
+  deleteContact: ctrlWrapper(deleteContact),
+  
+}
