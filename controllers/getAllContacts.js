d10f5d5fff21cd3ab06d3d0559d3b9cee30c8e90
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-06-09 23:16:16 +0300

    Fix: структура файлів

---
+++ controllers/getAllContacts.js
@@ -0,0 +1,17 @@
+const Contact = require("../models/contacts");
+const {ctrlWrapper} = require("../decorators")
+
+
+const getAllContacts = async (req, res, ) => {
+ 
+  const result = await Contact.find({}, "-__v");
+    res.json(result)
+  
+}
+
+     
+module.exports = {
+ 
+  getAllContacts: ctrlWrapper(getAllContacts),
+  
+}
