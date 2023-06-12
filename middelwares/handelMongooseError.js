02872fe8e439d4367bb31b3ead96e4db29449c1d
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-06-13 00:56:41 +0300

    create user routs

--- middelwares/handelMongooseError.js
+++ middelwares/handelMongooseError.js
@@ -1,5 +1,7 @@
 const handelMongooseError = (error, data, next) => {
-   error.status = 400;
+  const { code, name } = error;
+
+   error.status = (code === 1100 && name === "MongoServerError") ? 409 : 400;
   next();
 }
 
