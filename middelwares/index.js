8c77f99769f19b3568ef9b64b9f7b1dd4f609fbb
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-06-14 23:17:05 +0300

    finish homework

--- middelwares/index.js
+++ middelwares/index.js
@@ -1,10 +1,8 @@
 
-// const handelMongooseError = require("../helpers/handelMongooseError");
 const isValiId = require("./isValidId");
 const auteticate = require("./auteticate");
 
 module.exports = {
-    // handelMongooseError,
     isValiId,
     auteticate
 }
