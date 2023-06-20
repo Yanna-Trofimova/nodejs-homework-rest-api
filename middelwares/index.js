b158ec465e53c465b6e8089097b0dcfe30cc189e
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-06-20 17:36:01 +0300

    homework done

--- middelwares/index.js
+++ middelwares/index.js
@@ -1,9 +1,11 @@
 
 const isValiId = require("./isValidId");
 const auteticate = require("./auteticate");
+const upload = require("./upload");
 
 module.exports = {
     isValiId,
-    auteticate
+    auteticate,
+    upload,
 }
 
